edb9b8326ddc79b8a87a32480c7cc1d85c94eea1
Alessio De Pisi <alessiodepisi@gmail.com>
2021-02-01 15:38:58 +0100

    Trigger CI

--- tests/src/Context/EmailNotificationsContext.php
+++ tests/src/Context/EmailNotificationsContext.php
@@ -9,9 +9,10 @@ use Drupal\DrupalExtension\Context\RawDrupalContext;
  */
 class EmailNotificationsContext extends RawDrupalContext {
 
-  public $originalMailSystem;
-  public $activeEmail;
-  private $mailCollector = array();
+  private $originalMailSystem;
+  private $mailCollector = [];
+
+  const DRUPAL_MAIL_COLLECTOR = 'drupal_test_email_collector';
 
   /**
    * Prepare environment for TestingMailSystem getting the original mail system.
@@ -20,7 +21,27 @@ class EmailNotificationsContext extends RawDrupalContext {
    */
   public function setupEmailCollector() {
     // Store the original system to restore after the scenario.
-    $this->originalMailSystem = variable_get('mail_system', array('default-system' => 'MimeMailSystem'));
+    $this->originalMailSystem = variable_get('mail_system', ['default-system' => 'MimeMailSystem']);
+
+    // Set the test system.
+    variable_set('mail_system', ['default-system' => 'TestingMailSystem']);
+    // Flush the email buffer,
+    // allowing us to reuse this step definition to clear existing mail.
+    variable_set(self::DRUPAL_MAIL_COLLECTOR, []);
+
+    $this->mailCollector = [];
+  }
+
+  /**
+   * Clean testing email collector variable also after a scenario is finished.
+   *
+   * @AfterScenario @emails
+   */
+  public function cleanupEmailCollector() {
+    variable_del(self::DRUPAL_MAIL_COLLECTOR);
+    variable_set('mail_system', $this->originalMailSystem);
+
+    $this->mailCollector = [];
   }
 
   /**
@@ -33,41 +54,14 @@ class EmailNotificationsContext extends RawDrupalContext {
   }
 
   /**
-   * Enable the test mail system before testing email send.
-   *
-   * @Given /^the test email system is enabled$/
-   */
-  public function theTestEmailSystemIsEnabled() {
-    // Set the test system.
-    variable_set('mail_system', array('default-system' => 'TestingMailSystem'));
-    // Flush the email buffer,
-    // allowing us to reuse this step definition to clear existing mail.
-    variable_set('drupal_test_email_collector', array());
-  }
-
-  /**
    * Init property $mailCollector with system var drupal_test_email_collector.
    *
    * @Then /^I receive an email$/
    */
   public function iReceiveAnEmail() {
-
-    $variable_name = 'drupal_test_email_collector';
-
-    // We can't use variable_get() because $conf is only fetched once per
-    // scenario.
-    $variables = array_map('unserialize',
-      db_query("SELECT name, value FROM {variable} WHERE name = '$variable_name'")->fetchAllKeyed());
-
-    if (empty($variables['drupal_test_email_collector'])) {
-      throw new \Exception(sprintf('no emails in %s', $variable_name));
+    if (empty($this->getMailCollector())) {
+      throw new \Exception(sprintf('No emails collected'));
     }
-
-    foreach ($variables['drupal_test_email_collector'] as $message) {
-      $this->mailCollector[] = $message;
-    }
-
-    variable_del('drupal_test_email_collector');
   }
 
   /**
@@ -76,19 +70,15 @@ class EmailNotificationsContext extends RawDrupalContext {
    * @Then /^the email to "([^"]*)" should contain "([^"]*)"$/
    */
   public function theEmailToShouldContain($to, $contents) {
-    // We can't use variable_get() because $conf is only fetched once per
-    // scenario.
-    $variables = array_map('unserialize',
-      db_query("SELECT name, value FROM {variable} WHERE name = 'drupal_test_email_collector'")->fetchAllKeyed());
-    $this->activeEmail = FALSE;
+    $active_email = FALSE;
 
     // Parse $message array to search matching strings.
-    foreach ($variables['drupal_test_email_collector'] as $message) {
+    foreach ($this->getMailCollector() as $message) {
       if ($message['to'] == $to) {
-        $this->activeEmail = $message;
+        $active_email = $message;
         // Clean body from carriage return to search in clean strings.
-        $message['body'] = str_replace(array("\n", "\r"), ' ', $message['body']);
-        $message['subject'] = str_replace(array("\n", "\r"), ' ', $message['subject']);
+        $message['body'] = str_replace(["\n", "\r"], ' ', $message['body']);
+        $message['subject'] = str_replace(["\n", "\r"], ' ', $message['subject']);
         $found_in_body = strpos($message['body'], $contents);
         $found_in_subject = strpos($message['subject'], $contents);
 
@@ -100,7 +90,7 @@ class EmailNotificationsContext extends RawDrupalContext {
       }
     }
 
-    if ($this->activeEmail === FALSE) {
+    if ($active_email === FALSE) {
       // If string is not found in body or subject warn about negative result.
       throw new \Exception('Did not find expected content in message body or subject.');
     }
@@ -111,22 +101,28 @@ class EmailNotificationsContext extends RawDrupalContext {
   }
 
   /**
-   * Clean testing email collector variable also after a scenario is finished.
-   *
-   * @AfterScenario @emails
-   */
-  public function cleanupEmailCollector() {
-    variable_del('drupal_test_email_collector');
-    variable_set('mail_system', $this->originalMailSystem);
-  }
-
-  /**
-   * Get private variable $mailCollector.
+   * Get a list of the emails collected.
    *
    * @return array
    *   An array with all sent mails.
    */
   public function getMailCollector() {
+    // We can't use variable_get() because $conf is only fetched once per
+    // scenario.
+    $variables = array_map('unserialize',
+      db_query('SELECT name, value FROM {variable} WHERE name = :value',
+        [':value' => self::DRUPAL_MAIL_COLLECTOR]
+      )->fetchAllKeyed());
+
+    // Init property $mailCollector with system var drupal_test_email_collector.
+    if (!empty($variables[self::DRUPAL_MAIL_COLLECTOR])) {
+      foreach ($variables[self::DRUPAL_MAIL_COLLECTOR] as $message) {
+        $this->mailCollector[] = $message;
+      }
+    }
+
+    variable_del(self::DRUPAL_MAIL_COLLECTOR);
+
     return $this->mailCollector;
   }
 
